6a12c1d4e3ed78c2bb6b3f79386189d50403ad78
Rohan0004 <110601451+Rohan0004@users.noreply.github.com>
2023-10-01 20:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let str="";
+for (let i = 1; i < 101; i++) {
+	if(i%5 == 0 && i%3 == 0){
+		str+="FizzBuzz\n";
+	}else if (i%5 == 0) {
+		str+="Buzz\n";		
+	} else if (i%3==0) {
+		str+="Fizz\n";		
+	}else{
+		str+=i+"\n";
+	}
+}
+alert(str);
